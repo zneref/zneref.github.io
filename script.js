b046d4bb67e91a7e7eb4b513f60246379d63b209
zneref <zneref@gmail.com>
2018-04-12 21:09:36 +0200

    19.1 script.js fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'http://lit-inlet-46022.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://lit-inlet-46022.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
