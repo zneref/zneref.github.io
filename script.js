e6c319cec1e49fb6bd3ccafade24c81b4ba82066
zneref <zneref@gmail.com>
2018-05-10 20:29:18 +0200

    pop up removed

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ $(document).ready(function() {
       }),
       success: function(data) {
         console.log('Card created - ' + data.shortUrl);
-        alert('Card created - ' + data.shortUrl);
+        //alert('Card created - ' + data.shortUrl);
       }
     });
   }
